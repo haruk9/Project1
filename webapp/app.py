021287a431500765790a87d53d883f51b21b0198
Diogo Almeida <diommelal@gmail.com>
2022-08-08 15:45:30 +0100

    Update app and forms code.

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,8 @@
+from multiprocessing import connection
 from flask import Flask, render_template
-from pymongo import MongoClient
-from webapp.forms import TestForm
+import psycopg2
+from psycopg2 import Error
+from webapp.forms import RegForm
 
 
 app = Flask(__name__)
@@ -8,19 +10,37 @@ app.secret_key = 'secret stuff'
 
 
 
-@app.route('/')
-def hello_geek():
-    return render_template('base.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    reg_form = RegForm(meta={'csrf': False})
+    if reg_form.validate_on_submit():
+        try:
+            # Connect to an existing database
+            connection =psycopg2.connect(user="...", password="...", host="...", port="...", database="...")
 
-@app.route('/submit', methods=['GET', 'POST'])
-def submit():
-    form = TestForm()
-    if form.validate_on_submit():
-        name = {}
-        name['Name: '] = form.name.data
-        
-        
-    return render_template('submit.html', form=form)
+            cursor = connection.cursor()
+            username = reg_form.username.data
+            name = reg_form.name.data
+            email = reg_form.email.data
+            password = reg_form.password.data
+            # Call stored procedure
+            cursor.execute("...")
+            connection.commit()
+            cursor.close()
+
+            
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error while connectiong to PostgreSQL.", error)
+
+        finally:
+            #Closing connection
+            if connection:
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed")
+
+    return render_template('base.html', title='Registration', form=reg_form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- webapp/forms.py
+++ webapp/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo
 
-class TestForm(FlaskForm):
+class RegForm(FlaskForm):
+    username = StringField('Insert your Username', validators=[DataRequired()])
     name = StringField('Insert your Name', validators=[DataRequired()])
+    email = StringField('Insert your Name', validators=[DataRequired(), Email()])
+    password = PasswordField('Insert your Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Submit')
