aa4e56fb86cbd2f3a09a0b1c272231df1db27eb6
Diogo Almeida <diommelal@gmail.com>
2022-08-09 10:16:34 +0100

    Create HTML file and webpage.

--- webapp/app.py
+++ webapp/app.py
@@ -1,43 +1,39 @@
 from multiprocessing import connection
 from flask import Flask, render_template
-import psycopg2
-from psycopg2 import Error
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from webapp.forms import RegForm
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = '...'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 app.secret_key = 'secret stuff'
 
-
+class Regtable(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(25), index = True, unique = True)
+    name = db.Column(db.String(50), index = True, unique = False)
+    email = db.Column(db.String(50), index = True, unique = True)
+    password = db.Column(db.String(50), index = True, unique = False)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     reg_form = RegForm(meta={'csrf': False})
     if reg_form.validate_on_submit():
-        try:
-            # Connect to an existing database
-            connection =psycopg2.connect(user="...", password="...", host="...", port="...", database="...")
+        
+        username = reg_form.username.data
+        name = reg_form.name.data
+        email = reg_form.email.data
+        password = reg_form.password.data
 
-            cursor = connection.cursor()
-            username = reg_form.username.data
-            name = reg_form.name.data
-            email = reg_form.email.data
-            password = reg_form.password.data
-            # Call stored procedure
-            cursor.execute("...")
-            connection.commit()
-            cursor.close()
+        user = Regtable(username = username, name = name, email = email, password=password)
 
-            
-        except (Exception, psycopg2.DatabaseError) as error:
-            print("Error while connectiong to PostgreSQL.", error)
-
-        finally:
-            #Closing connection
-            if connection:
-                cursor.close()
-                connection.close()
-                print("PostgreSQL connection is closed")
+        db.session.add(user)
+        db.session.commit()
 
     return render_template('base.html', title='Registration', form=reg_form)
 
